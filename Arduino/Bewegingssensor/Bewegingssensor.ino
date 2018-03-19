7b74bb1105cc55e601facc9a5946f361548f2265
Rovexx <32699476+Rovexx@users.noreply.github.com>
2018-03-19 13:04:18 +0100

    0.11
    arduino code aangepast en in map geplaatst

--- Arduino/Bewegingssensor/Bewegingssensor.ino
+++ Arduino/Bewegingssensor/Bewegingssensor.ino
@@ -2,10 +2,10 @@ const int trigPin = 9;
 const int echoPin = 10;
 
 long duration;
-int distance;
-int distances[] = {};
-int distanceCount;
-int detectedCount;
+int distance = 0;;
+int distances[5];
+int distanceCount = 0;
+int detectedCount = 0;
 
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
@@ -13,16 +13,18 @@ void setup() {
   Serial.begin(9600); // Starts the serial communication
 }
 
-void loop() {
+void loop() 
+{
   // 5 of the same distances for insurance
-  if(distanceCount == 4){
+  if(distanceCount == 5){
     for(int x = 0; x < 5; x++){
-      if(5 < distances[x] < 15){
-          if(detectedCount == 4){
+      Serial.println(detectedCount);
+      if(distances[x] < 15){
+          if(detectedCount == 5){
             Serial.println("Trein gedetecteerd");
             detectedCount = 0;
           }
-          detectedCount = detectedCount + 1;
+          detectedCount ++;
       }else{
         detectedCount = 0;
       }
@@ -30,7 +32,18 @@ void loop() {
     distanceCount = 0;
   }else{
     for(int i = 0; i < 5; i++) {
-      // Clears the trigPin
+      afstandmeting();
+      delay(1000);
+  
+      distances[i] = distance;
+      distanceCount ++;
+    }
+  }
+}
+
+void afstandmeting()
+{
+  // Clears the trigPin
       digitalWrite(trigPin, LOW);
       
       // Sets the trigPin on HIGH state for 10 micro seconds
@@ -48,11 +61,5 @@ void loop() {
       Serial.println(distance);
       Serial.print("Distancecount: ");
       Serial.println(distanceCount);
-      delay(1000);
-  
-      distances[i] = distance;
-      distanceCount = distanceCount + 1;
-    }
-  }
 }
 
