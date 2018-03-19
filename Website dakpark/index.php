a46c9cff30b8023f8addc7736ad27de6a7641d59
JeroenvanderRhee <32259558+JeroenvanderRhee@users.noreply.github.com>
2018-03-19 14:14:56 +0100

    0.0.12
    Eternal score

--- Website dakpark/index.php
+++ Website dakpark/index.php
@@ -4,38 +4,60 @@
 		<link rel="stylesheet" href="Css/Stylesheet.css" type="text/css"/>
 		<script type="text/javascript" src="Js/Slider.js">
 		</script>
-		<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=PT+Sans|Roboto+Slab" rel="stylesheet">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<title>Website Dakpark</title>
 	</head>
 	<body>
 		<div class="slide1 container Slides">
-			<div class="part-50">
-				<h1 class="headtitle">Dag score:</h1>
+			<div class="part-50 wrapper-right-score">
+
+				<h1 class="headtitle">Highscore allertijden:</h1>
 				<div class="body-score">
 					<div class="containernames">
 						<p class="paragraphnames">
-							<ol>
-								<li>Karel</li>
-								<li>Pieter</li>
-								<li>Jacob</li>
+							<ol class="ol-eternal-score">
+								<li class="paragraph-eternal-highscore">Jantje</li>
 							</ol>
 						</p>
 					</div>
 					<div class="containerscore">
 						<p class="paragraphscore">
 							<ul>
-								<li>30</li>
-								<li>20</li>
-								<li>10</li>
+								<li class="paragraph-eternal-highscore">999</li>
+							</ul>
+						</p>
+					</div>
+				</div>
+
+
+
+
+				<h1 class="headtitle">Dag score:</h1>
+				<div class="body-score">
+					<div class="containernames">
+						<p class="paragraphnames">
+							<ol>
+								<li class="paragraph bold-highscore">Karel</li>
+								<li class="paragraph">Pieter</li>
+								<li class="paragraph">Jacob</li>
+							</ol>
+						</p>
+					</div>
+					<div class="containerscore">
+						<p class="paragraphscore">
+							<ul>
+								<li class="paragraph bold-highscore">30</li>
+								<li class="paragraph">20</li>
+								<li class="paragraph">10</li>
 							</ul>
 						</p>
 					</div>
 				</div>
 			</div>
 		</div>
-<!-- 		<div class="slide2 container Slides">
+<!--  		<div class="slide2 container Slides">
 		</div>
 		<div class="slide3 container Slides">
 		</div> -->
