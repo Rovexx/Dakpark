e662c7b86c1becf956f54a8cf4fb908f74fac955
JeroenvanderRhee <32259558+JeroenvanderRhee@users.noreply.github.com>
2018-03-19 13:48:50 +0100

    0.0.12
    Scoreboard

--- Website dakpark/index.php
+++ Website dakpark/index.php
@@ -4,16 +4,41 @@
 		<link rel="stylesheet" href="Css/Stylesheet.css" type="text/css"/>
 		<script type="text/javascript" src="Js/Slider.js">
 		</script>
+		<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 		<title>Website Dakpark</title>
 	</head>
 	<body>
 		<div class="slide1 container Slides">
+			<div class="part-50">
+				<h1 class="headtitle">Dag score:</h1>
+				<div class="body-score">
+					<div class="containernames">
+						<p class="paragraphnames">
+							<ol>
+								<li>Karel</li>
+								<li>Pieter</li>
+								<li>Jacob</li>
+							</ol>
+						</p>
+					</div>
+					<div class="containerscore">
+						<p class="paragraphscore">
+							<ul>
+								<li>30</li>
+								<li>20</li>
+								<li>10</li>
+							</ul>
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
-		<div class="slide2 container Slides">
+<!-- 		<div class="slide2 container Slides">
 		</div>
 		<div class="slide3 container Slides">
-		</div>
+		</div> -->
 		<script>
 			Timer();
 		</script>
