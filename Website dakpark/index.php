a718df8fb7fa6d8006b5a646b183562bcfb267c1
JeroenvanderRhee <32259558+JeroenvanderRhee@users.noreply.github.com>
2018-03-19 10:05:26 +0100

    0.0.4
    Css en JS toegevoegd

---
+++ Website dakpark/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="Css/Stylesheet.css" type="text/css"/>
+		<title>Website Dakpark</title>
+	</head>
+	<body>
+		<div class="slide1 container">
+
+		</div>
+		<div class="slide2 container">
+
+		</div>
+		<div class="slide3 container">
+
+		</div>
+		<script type="text/javascript" src="Js/Slider.js" />
+	</body>
+</html>
